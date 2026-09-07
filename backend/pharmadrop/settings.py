33eadcb39f31cbcf9406e62e814e3b5bbee6013b
BarasaIan11 <ebarasa845@gmail.com>
2026-09-07 22:41:40 +0300

    fix: add a LOGGING config to settings.py that forces errors to stdout/stderr regardless of DEBUG.

--- backend/pharmadrop/settings.py
+++ backend/pharmadrop/settings.py
@@ -191,3 +191,30 @@ PUSHER_KEY = os.getenv('PUSHER_KEY', '')
 PUSHER_SECRET = os.getenv('PUSHER_SECRET', '')
 PUSHER_CLUSTER = os.getenv('PUSHER_CLUSTER', 'mt1')
 PUSHER_SSL     = True 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
+
